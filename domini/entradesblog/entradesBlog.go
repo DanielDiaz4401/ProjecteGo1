a723869e7e91296d4108d73ec7b35ac10fd6e3dc
DanielDiaz4401 <danieldiaz4401@gmail.com>
2020-05-09 03:25:11 +0200

    Falta ordena

--- domini/entradesblog/entradesBlog.go
+++ domini/entradesblog/entradesBlog.go
@@ -4,6 +4,10 @@
 // ordre segons el criteri
 package entradesblog
 
+import(
+	"github.com/DanielDiaz4401/ProjecteGo1/domini/entrada"
+)
+
 // Llista d'entrades de l'usuari
 type EntradesBlog []Entrada
 
@@ -16,18 +20,62 @@ func New() EntradesBlog {
 // Agafa l'entrada amb id==num i,
 // si existeix, la retorna.
 func (e *EntradesBlog) Agafa(num int) Entrada {
-	for _, var := range e {
+	for _,var := range e {
 		if var.GetID()==num {
 			return var
 		}
-		return nil
 	}
+	return nil
 }
 
 // Elimina l'entrada amb id==num i,
 // si existeix, la retorna
 func (e *EntradesBlog) Elimina(num int) Entrada {
 	for i, var := range e {
-		
+		if var.GetID()==num {
+			remove(e,i)
+			return var
+		}
 	}
+	return nil
+}
+
+// EntreDates retorna un string amb les
+// compreses entre les dues dates.
+// "Dates{
+//		entrada
+// }"
+func (e *EntradesBlog) EntreDates(inici,fi temps.Temps) string{
+	aux := "Dates{\n"
+	for _, var := range e {
+		aux +="\t" var.GetID()+" "+var.GetTitol+"\n" 
+	}
+	return aux+"}"
+}
+
+// Index retorna un string amb les
+// entrades ordenades.
+// "Index{
+//		id titol
+// }"
+func (e *EntradesBlog) Index() string{
+	aux := "Index{\n"
+	for _, var := range e {
+		aux +="\t" var.GetID()+" "+var.GetTitol+"\n" 
+	}
+	return aux+"}"
+}
+
+// ToString retorna un string amb les
+// entrades ordenades.
+// "Totes{
+//		entrada
+// }"
+func (e *EntradesBlog) ToString() string{
+	var aux string
+	aux := "Totes{\n"
+	for _, var := range e {
+		aux += var.GetID()+" "+var.GetTitol()+var.GetQuan()"\n" 
+	}
+	return aux+"}"
 }
