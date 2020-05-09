3fa6b1b11ac422edf2dfb82375bb9d1221e860ee
DanielDiaz4401 <danieldiaz4401@gmail.com>
2020-05-09 18:04:03 +0200

    Acabado

--- domini/entradesblog/entradesBlog.go
+++ domini/entradesblog/entradesBlog.go
@@ -19,43 +19,65 @@ type Entrada = entrada.Entrada
 
 // EntradesBlog es d'entrades
 // de l'usuari
-type EntradesBlog []Entrada
+type EntradesBlog struct {
+	entrades []entrada.Entrada
+}
 
 var criteri string
 
 // New crea una llista buida d'entrades
 func New() EntradesBlog {
-	return []Entrada{}
+	return EntradesBlog{[]entrada.Entrada{}}
+}
+
+// AfegeixOrdenat afegeix una Entrada e a la posició
+// que li correspon a la llista d'entrades,
+// segons el criteri actual.
+func (e *EntradesBlog) AfegeixOrdenat(in entrada.Entrada) {
+	for i, aux := range e.entrades {
+		if i < len(e.entrades)-1 {
+			if entrada.Compare(in, aux, criteri) < 0 {
+				e.entrades = remove(e.entrades, i)
+				return
+			}
+		}
+	}
+	e.entrades = append(e.entrades, in)
+}
+
+func add(slice []entrada.Entrada, i int, afegir entrada.Entrada) []entrada.Entrada {
+	aux := append(slice[:i], afegir)
+	return append(aux, slice[i+1:]...)
 }
 
 // Agafa l'entrada amb id==num.
 // Si existeix, la retorna.
 // Si no existeix, retorna err
-func (e *EntradesBlog) Agafa(num int) (Entrada, error) {
-	for _, entrada := range *e {
+func (e *EntradesBlog) Agafa(num int) (entrada.Entrada, error) {
+	for _, entrada := range e.entrades {
 		if entrada.GetID() == num {
 			return entrada, nil
 		}
 	}
-	return Entrada{}, errors.New("No existeix l'entrada")
+	return entrada.Entrada{}, errors.New("No existeix l'entrada")
 }
 
 // Elimina l'entrada amb id==num.
 // Si existeix, la retorna.
 // Si no existeix, retorna err
-func (e *EntradesBlog) Elimina(num int) (Entrada, error) {
-	for i, entrada := range *e {
+func (e *EntradesBlog) Elimina(num int) (entrada.Entrada, error) {
+	for i, entrada := range e.entrades {
 		if entrada.GetID() == num {
-			*e = remove(*e, i)
+			e.entrades = remove(e.entrades, i)
 			return entrada, nil
 		}
 	}
-	return Entrada{}, errors.New("No existeix l'entrada")
+	return entrada.Entrada{}, errors.New("No existeix l'entrada")
 }
 
 // Ejemplo de polimorfismo en go
-func remove(slice []Entrada, s int) []Entrada {
-	return append(slice[:s], slice[s+1:]...)
+func remove(slice []entrada.Entrada, i int) []entrada.Entrada {
+	return append(slice[:i], slice[i+1:]...)
 }
 
 // EntreDates retorna un string amb les
@@ -65,7 +87,7 @@ func remove(slice []Entrada, s int) []Entrada {
 // }"
 func (e *EntradesBlog) EntreDates(inici, fi temps.Temps) string {
 	aux := "Dates{\n"
-	for _, entrada := range *e {
+	for _, entrada := range e.entrades {
 		if entrada.GetQuan().ComparaTemps(inici) > 0 {
 			if entrada.GetQuan().ComparaTemps(fi) < 0 {
 				aux += "\t" + strconv.Itoa(int(entrada.GetID())) + " " + entrada.GetTitol() + "\n"
@@ -82,7 +104,7 @@ func (e *EntradesBlog) EntreDates(inici, fi temps.Temps) string {
 // }"
 func (e *EntradesBlog) Index() string {
 	aux := "Index{\n"
-	for _, entrada := range *e {
+	for _, entrada := range e.entrades {
 		aux += "\t" + strconv.Itoa(int(entrada.GetID())) + " " + entrada.GetTitol() + "\n"
 	}
 	return aux + "}"
@@ -95,7 +117,7 @@ func (e *EntradesBlog) Index() string {
 // }"
 func (e *EntradesBlog) ToString() string {
 	aux := "Totes{\n"
-	for _, entrada := range *e {
+	for _, entrada := range e.entrades {
 		aux += "\t" + strconv.Itoa(int(entrada.GetID())) + " " + entrada.GetTitol() + entrada.GetQuan().Get() + "\n"
 	}
 	return aux + "}"
